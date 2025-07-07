894dbc1d96a10490dd8ef856d3403502cfcdef55
mtunsnef <tuannm23.dev@gmail.com>
2025-07-07 21:26:09 +0700

    chore: update models/dtbs

--- FashionShopSystem.Domain/Models/OrderDetail.cs
+++ FashionShopSystem.Domain/Models/OrderDetail.cs
@@ -1,18 +1,21 @@
-﻿namespace FashionShopSystem.Domain.Models;
+﻿using System;
+using System.Collections.Generic;
+
+namespace FashionShopSystem.Domain.Models;
 
 public partial class OrderDetail
 {
-	public int OrderDetailId { get; set; }
+    public int OrderDetailId { get; set; }
 
-	public int? OrderId { get; set; }
+    public int? OrderId { get; set; }
 
-	public int? ProductId { get; set; }
+    public int? ProductId { get; set; }
 
-	public int? Quantity { get; set; }
+    public int? Quantity { get; set; }
 
-	public decimal? Price { get; set; }
+    public decimal? Price { get; set; }
 
-	public virtual Order? Order { get; set; }
+    public virtual Order? Order { get; set; }
 
-	public virtual Product? Product { get; set; }
+    public virtual Product? Product { get; set; }
 }
--- FashionShopSystem.Domain/Models/User.cs
+++ FashionShopSystem.Domain/Models/User.cs
@@ -1,26 +1,29 @@
-﻿namespace FashionShopSystem.Domain.Models;
+﻿using System;
+using System.Collections.Generic;
+
+namespace FashionShopSystem.Domain.Models;
 
 public partial class User
 {
-	public int UserId { get; set; }
+    public int UserId { get; set; }
 
-	public string? FullName { get; set; }
+    public string? FullName { get; set; }
 
-	public string? Email { get; set; }
+    public string? Email { get; set; }
 
-	public string? PasswordHash { get; set; }
+    public string? PasswordHash { get; set; }
 
-	public string? Phone { get; set; }
+    public string? Phone { get; set; }
 
-	public string? Address { get; set; }
+    public string? Address { get; set; }
 
-	public string? Role { get; set; }
+    public string? Role { get; set; }
 
-	public DateTime? CreatedAt { get; set; }
+    public DateTime? CreatedAt { get; set; }
 
-	public bool? IsActive { get; set; }
+    public bool? IsActive { get; set; }
 
-	public virtual ICollection<Favorite> Favorites { get; set; } = new List<Favorite>();
+    public virtual ICollection<Favorite> Favorites { get; set; } = new List<Favorite>();
 
-	public virtual ICollection<Order> Orders { get; set; } = new List<Order>();
+    public virtual ICollection<Order> Orders { get; set; } = new List<Order>();
 }
