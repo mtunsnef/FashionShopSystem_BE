cf235ddc4c8b0e342000fb49a42234d16cc42b6b
mtunsnef <tuannm23.dev@gmail.com>
2025-07-11 12:14:38 +0700

    feat: add order detail service

---
+++ FashionShopSystem.Infrastructure/Repositories/OrderDetailRepo/IOrderDetailRepository.cs
@@ -0,0 +1,13 @@
+﻿using FashionShopSystem.Domain.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FashionShopSystem.Infrastructure.Repositories.OrderDetailRepo
+{
+    public interface IOrderDetailRepository : IRepositoryBase<OrderDetail>
+    {
+    }
+}
---
+++ FashionShopSystem.Infrastructure/Repositories/OrderDetailRepo/OrderDetailRepository.cs
@@ -0,0 +1,17 @@
+﻿using FashionShopSystem.Domain.Models;
+using FashionShopSystem.Infrastructure.Repositories.OrderRepo;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FashionShopSystem.Infrastructure.Repositories.OrderDetailRepo
+{
+    public class OrderDetailRepository : RepositoryBase<OrderDetail>, IOrderDetailRepository
+    {
+        public OrderDetailRepository(FashionShopContext context) : base(context)
+        {
+        }
+    }
+}
