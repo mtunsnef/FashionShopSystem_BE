43675a0ea7f0fc6fc2137ecbd721e69d8daa660f
mtunsnef <tuannm23.dev@gmail.com>
2025-07-27 22:38:25 +0700

    feat: add two factor auth (for user) feature

---
+++ FashionShopSystem.Service/DTOs/AuthDto/TwoFaInitDto.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FashionShopSystem.Service.DTOs.AuthDto
+{
+    public class TwoFaInitDto
+    {
+        public string SharedKey { get; set; }
+        public string QrCodeUri { get; set; }
+    }
+}
